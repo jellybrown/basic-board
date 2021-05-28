2258474c49290daabab2f034a14442b94f7de9a5
yujin mac <kimyu28jin@gmail.com>
2021-05-28 13:44:30 +0900

    Form 컴포넌트 추가
    - 기본 폼 스타일 작성

--- front/src/components/Form/Form.js
+++ front/src/components/Form/Form.js
@@ -1,24 +1,52 @@
 import React, { useState } from "react";
+import { Content } from "../Detail/DetailElement";
 //import { faUser } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FormWrapper } from "./FormElement";
+import { FormWrapper, Column, Row, Title, ColumnWrapper } from "./FormElement";
 const Form = () => {
   const [text, setText] = useState();
   const onChange = (e) => {
     setText(e.target.value);
   };
   return (
-    <FormWrapper className="field">
-      <label className="label">이름</label>
-      <div className="name__input">
-        <input
-          className="input is-success"
-          type="text"
-          placeholder="김멍멍"
-          value={text}
-          onChange={onChange}
-        />
-      </div>
+    <FormWrapper>
+      <Row className="field">
+        <Title className="label">작성자</Title>
+        <ColumnWrapper>
+          <div className="control">
+            <input className="input" type="text" placeholder="김모모" />
+          </div>
+          <div className="control">
+            <input
+              className="input"
+              type="email"
+              placeholder="hello@gmail.com"
+            />
+          </div>
+        </ColumnWrapper>
+      </Row>
+      <Row className="field">
+        <Title className="label">게시글 비밀번호</Title>
+        <Column className="control">
+          <input className="input" type="text" placeholder="ex) 123456" />
+        </Column>
+      </Row>
+      <Row className="field">
+        <Title className="label">제목</Title>
+        <Column className="control">
+          <input className="input" type="text" placeholder="ex) 안녕하세요." />
+        </Column>
+      </Row>
+      <Row>
+        <Title className="label">내용</Title>
+        <Column>
+          <textarea
+            class="textarea"
+            placeholder="내용을 입력해주세요."
+            rows="8"
+          ></textarea>
+        </Column>
+      </Row>
     </FormWrapper>
   );
 };
--- front/src/components/Form/FormElement.js
+++ front/src/components/Form/FormElement.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
 export const FormWrapper = styled.div``;
+
+export const Row = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const Title = styled.label`
+  width: 150px;
+`;
+
+export const ColumnWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+
+  .control {
+    flex-basis: 50%;
+  }
+`;
+
+export const Column = styled.div`
+  flex: 1;
+`;
+
+export const Content = styled.div`
+  flex: 1;
+`;

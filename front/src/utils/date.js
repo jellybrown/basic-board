7073a1fe7a044d4c54a186abfc31aaaa79647a23
yujin mac <kimyu28jin@gmail.com>
2021-06-12 22:33:30 +0900

    날짜관련 로직 추가
    - dayjs install
    - 한글 날짜 로직 추가

---
+++ front/src/utils/date.js
@@ -0,0 +1,26 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
+const isToday = (date) => {
+  const today = new Date();
+  return (
+    date.getDate() === today.getDate() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear()
+  );
+};
+
+const checkToday = (createdAt) => {
+  const date = new Date(createdAt);
+  return isToday(date);
+};
+
+export const changeDate = (date) => {
+  const isWriteToday = checkToday(date);
+  if (isWriteToday) return dayjs(date).fromNow();
+  else return dayjs(date).format("YYYY.MM.DD");
+};

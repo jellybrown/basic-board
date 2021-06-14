7f44a9ceb730f623eae48863c9e5cf842b7a3cf7
yujin mac <kimyu28jin@gmail.com>
2021-06-14 16:13:31 +0900

    글 수정 api 추가
    - 확인 완료

--- back/routes/postRoute.js
+++ back/routes/postRoute.js
@@ -55,4 +55,42 @@ postRouter.post("/", async (req, res) => {
   }
 });
 
+// 수정,삭제,읽기시 필요한 비밀번호 확인 따로 빼기
+
+// PUT api/post/:postId
+postRouter.put("/:postId", async (req, res) => {
+  try {
+    const { postId } = req.params;
+    if (!isValidObjectId(postId))
+      return res.status(400).send("게시글이 존재하지 않습니다.");
+    const { name, email, password, title, content } = req.body;
+    const updatedPost = {};
+    if (name || email) updatedPost.user = { name, email };
+    if (password) updatedPost.password = password;
+    if (title) updatedPost.title = title;
+    if (content) updatedPost.content = content;
+    const post = await Post.findByIdAndUpdate(postId, updatedPost, {
+      new: true,
+    });
+    return res.send({ post });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err: err.message });
+  }
+});
+
+// DELETE api/post/:postId
+postRouter.delete("/:postId", async (req, res) => {
+  try {
+    const { postId } = req.params;
+    if (!isValidObjectId(postId))
+      return res.status(400).send("게시글이 존재하지 않습니다.");
+    const post = await Post.findOneAndDelete({ _id: postId });
+    return res.send({ post });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err: err.message });
+  }
+});
+
 module.exports = { postRouter };

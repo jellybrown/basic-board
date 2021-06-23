6937a936182588236e748cfadee4fdb7ff8344c5
yujin mac <kimyu28jin@gmail.com>
2021-06-23 21:16:54 +0900

    load post api 수정
    - 생성일 순으로 수정

--- back/routes/postRoute.js
+++ back/routes/postRoute.js
@@ -1,14 +1,14 @@
-const { Router } = require("express");
-const { Post } = require("../models/post");
-const { isValidObjectId } = require("mongoose");
+const { Router } = require('express');
+const { Post } = require('../models/post');
+const { isValidObjectId } = require('mongoose');
 const postRouter = Router();
 
 // GET api/post/:postIid
-postRouter.get("/:postId", async (req, res) => {
+postRouter.get('/:postId', async (req, res) => {
   try {
     const { postId } = req.params;
     if (!isValidObjectId(postId))
-      return res.status(400).send("포스트를 찾을 수 없습니다.");
+      return res.status(400).send('포스트를 찾을 수 없습니다.');
     const post = await Post.findOne({ _id: postId });
     return res.send({ post });
   } catch (err) {
@@ -18,13 +18,13 @@ postRouter.get("/:postId", async (req, res) => {
 });
 
 // GET api/post
-postRouter.get("/", async (req, res) => {
+postRouter.get('/', async (req, res) => {
   try {
     let { page } = req.query;
     page = parseInt(page);
     let postsCount = await Post.find({}).countDocuments();
     let posts = await Post.find({})
-      .sort({ updatedAt: -1 })
+      .sort({ createdAt: -1 })
       .skip(page * 5)
       .limit(5);
     return res.send({ posts, postsCount });
@@ -35,22 +35,22 @@ postRouter.get("/", async (req, res) => {
 });
 
 // POST api/post
-postRouter.post("/", async (req, res) => {
+postRouter.post('/', async (req, res) => {
   try {
     console.log(req.body);
     const { user, password, title, content } = req.body;
     if (
-      typeof user !== "object" ||
-      typeof user.name !== "string" ||
-      typeof user.email !== "string"
+      typeof user !== 'object' ||
+      typeof user.name !== 'string' ||
+      typeof user.email !== 'string'
     )
-      return res.status(400).send("user 정보를 확인해주세요.");
-    if (typeof password !== "string")
-      return res.status(400).send("password 정보를 확인해주세요.");
-    if (typeof title !== "string")
-      return res.status(400).send("title 정보를 확인해주세요.");
-    if (typeof content !== "string")
-      return res.status(400).send("content 정보를 확인해주세요.");
+      return res.status(400).send('user 정보를 확인해주세요.');
+    if (typeof password !== 'string')
+      return res.status(400).send('password 정보를 확인해주세요.');
+    if (typeof title !== 'string')
+      return res.status(400).send('title 정보를 확인해주세요.');
+    if (typeof content !== 'string')
+      return res.status(400).send('content 정보를 확인해주세요.');
     let post = await new Post({ ...req.body });
     await post.save();
     return res.send({ post });
@@ -63,11 +63,11 @@ postRouter.post("/", async (req, res) => {
 // 수정,삭제,읽기시 필요한 비밀번호 확인 따로 빼기
 
 // PUT api/post/:postId
-postRouter.put("/:postId", async (req, res) => {
+postRouter.put('/:postId', async (req, res) => {
   try {
     const { postId } = req.params;
     if (!isValidObjectId(postId))
-      return res.status(400).send("게시글이 존재하지 않습니다.");
+      return res.status(400).send('게시글이 존재하지 않습니다.');
     const { name, email, password, title, content } = req.body;
     const updatedPost = {};
     if (name || email) updatedPost.user = { name, email };
@@ -85,11 +85,11 @@ postRouter.put("/:postId", async (req, res) => {
 });
 
 // DELETE api/post/:postId
-postRouter.delete("/:postId", async (req, res) => {
+postRouter.delete('/:postId', async (req, res) => {
   try {
     const { postId } = req.params;
     if (!isValidObjectId(postId))
-      return res.status(400).send("게시글이 존재하지 않습니다.");
+      return res.status(400).send('게시글이 존재하지 않습니다.');
     const post = await Post.findOneAndDelete({ _id: postId });
     return res.send({ post });
   } catch (err) {

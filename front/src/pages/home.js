067ab19e5e165b8ed91ae0df43b91f212fb5bd56
yujin mac <kimyu28jin@gmail.com>
2021-06-23 21:16:31 +0900

    Home에서 loading UI 추가

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadPosts } from '../redux/PostsSlice';
 import { ButtonWrapper } from '../components/Form/FormElement';
+import { useState } from 'react';
+import { Loading, LoadingWrapper } from '../styles/styles';
 
 const Home = () => {
   const page = location.search;
@@ -11,17 +13,26 @@ const Home = () => {
   const result = page.match(regex) || [0]; // ?page=2 숫자만 꺼내기
   const posts = useSelector((state) => state.posts.posts);
   const postsCount = useSelector((state) => state.posts.postsCount);
+  const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(loadPosts(result[0]));
-    console.log(posts);
+  useEffect(async () => {
+    await setLoading(true);
+    await dispatch(loadPosts(result[0]));
+    await setLoading(false);
   }, [result[0]]);
 
   return (
     <>
-      <List posts={posts} postsCount={postsCount} />
+      {loading ? (
+        <LoadingWrapper>
+          <Loading></Loading>
+        </LoadingWrapper>
+      ) : (
+        <List posts={posts} postsCount={postsCount} />
+      )}
+
       <ButtonWrapper>
         <Link to="/write">
           <button className="button is-info">글쓰기</button>
